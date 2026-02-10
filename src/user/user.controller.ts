3554949019d0ed00ec2312143e121d9d55720143
HighRol1er <hunterjoe9999@gmail.com>
2026-02-10 23:33:26 +0900

    chore: clear user module

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,30 +1,4 @@
-import { Controller, Get, Post } from '@nestjs/common';
-import { UserService } from './user.service';
+import { Controller } from '@nestjs/common';
 
 @Controller('user')
-export class UserController {
-  constructor(private readonly userService: UserService) {}
-
-  @Get()
-  async getUsers(): Promise<any> {
-    const users = await this.userService.getUsers();
-    if (users.length === 0) {
-      return {
-        message: 'No users found',
-        status: 404,
-      };
-    }
-    return {
-      message: 'Users found',
-      data: users,
-    };
-  }
-
-  @Post()
-  async insertUser(): Promise<any> {
-    await this.userService.insertUser();
-    return {
-      message: 'User inserted',
-    };
-  }
-}
+export class UserController {}
