39ca7628b711b71c43e97827f7415e45f8d3c66c
HighRol1er <hunterjoe9999@gmail.com>
2026-02-15 08:53:50 +0900

    refactor: 유저 찾기 분리

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,18 +1,23 @@
 import type { DB } from '@/db/db.module';
+import type { NewUser, User } from '@/db/schema';
 
-import { Injectable, Inject } from '@nestjs/common';
-import { NewUser, users } from '@/db/schema';
+import { users } from '@/db/schema';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 import { eq } from 'drizzle-orm';
-
-// This should be a real class/interface representing a user entity
 
 @Injectable()
 export class UserService {
   private readonly userSchema = users;
+  private readonly logger = new Logger();
   constructor(@Inject('DATABASE') private readonly db: DB) {}
 
+  async findUser(email: string): Promise<User | undefined> {
+    const [existingUser] = await this.db.select().from(this.userSchema).where(eq(users.email, email));
+    return existingUser;
+  }
+
   async findOrCreateUser(data: NewUser) {
-    const [existingUser] = await this.db.select().from(this.userSchema).where(eq(users.email, data.email));
+    const existingUser = await this.findUser(data.email);
 
     if (existingUser) return existingUser;
 
